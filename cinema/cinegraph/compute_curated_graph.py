534771f2e8880f2f7b86c4bde5ed4c8477820a82
Reuben Brasher <rkbrasher@gmail.com>
2024-08-31 18:39:10 -0700

    Display the path of a game from a curated collection of actors and movies

--- cinema/cinegraph/compute_curated_graph.py
+++ cinema/cinegraph/compute_curated_graph.py
@@ -1,5 +1,7 @@
 import pandas as pd
+import networkx as nx
 from cinema.cinegraph.node_types import PersonNode, WorkNode
+from cinema.cinegame.game_maker import GameMaker
 
 
 class Curator:
@@ -10,6 +12,14 @@ class Curator:
         self.movie_titles = movie_titles
         self.actors = {name: self.actor_lookup(name) for name in actor_names}
         self.movies = {title: self.movie_lookup(title) for title in movie_titles}
+        self.actor_by_id = {
+            int(self.actors[name].getID()): name for name in self.actor_names
+        }
+        self.movie_by_id = {
+            int(self.movies[title].getID()): title for title in self.movie_titles
+        }
+        self.curated_graph = self.make_curated_graph()
+        self.game_maker = GameMaker(self.curated_graph)
 
     def movie_lookup(self, title):
         movies = self.ia.search_movie(title)
@@ -24,12 +34,18 @@ class Curator:
         return actors[0]
 
     def make_movie_df(self):
-        ids = pd.Series([self.movies[title].getID() for title in self.movie_titles], dtype=str)
-        years = [self.movies[title].data.get("year", None) for title in self.movie_titles]
+        ids = pd.Series(
+            [self.movies[title].getID() for title in self.movie_titles], dtype=str
+        )
+        years = [
+            self.movies[title].data.get("year", None) for title in self.movie_titles
+        ]
         return pd.DataFrame({"IMDb_ID": ids, "year": years, "title": self.movie_titles})
 
     def make_actor_df(self):
-        ids = pd.Series([self.actors[name].getID() for name in self.actor_names], dtype=str)
+        ids = pd.Series(
+            [self.actors[name].getID() for name in self.actor_names], dtype=str
+        )
         return pd.DataFrame({"IMDb_ID": ids, "name": self.actor_names})
 
     def make_curated_graph(self):
@@ -39,3 +55,14 @@ class Curator:
         movie_nodes = [WorkNode(int(i)) for i in movie_ids]
 
         return self.g.subgraph(actor_nodes + movie_nodes)
+
+    def plot_path(self, a, b):
+        def lookup(n):
+            if n.is_person:
+                return self.actor_by_id[n.id]
+            else:
+                return self.movie_by_id[n.id]
+
+        path = nx.shortest_path(self.curated_graph, a, b)
+
+        return [lookup(n) for n in path]
